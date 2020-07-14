ddaaca24b41e96880f542123f2272ac997cda064
TOMOFUMI KONDO <ugax2kontomo0314@gmai.com>
2020-07-14 22:22:36 +0900

    ログインのドメイン指定方法の変更

--- src/firebase.js
+++ src/firebase.js
@@ -1,60 +1,67 @@
 import firebase from "firebase/app";
 import "firebase/auth";
-import "firebase/analytics"
+import "firebase/analytics";
 import store from "./store";
 import router from "./router";
 
 // todo: 環境変数に記述する
 const firebaseConfig = {
-    apiKey: "AIzaSyCi4s17WSq1AP9ZbLfoO15quBXIpFaAHSY",
-    authDomain: "univ-seminer-unificate.firebaseapp.com",
-    databaseURL: "https://univ-seminer-unificate.firebaseio.com",
-    projectId: "univ-seminer-unificate",
-    storageBucket: "univ-seminer-unificate.appspot.com",
-    messagingSenderId: "1030793997557",
-    appId: "1:1030793997557:web:b54826b6ecde22f5fbe9bc",
-    measurementId: "G-QKQ5Q6PX0S"
+  apiKey: "AIzaSyCi4s17WSq1AP9ZbLfoO15quBXIpFaAHSY",
+  authDomain: "univ-seminer-unificate.firebaseapp.com",
+  databaseURL: "https://univ-seminer-unificate.firebaseio.com",
+  projectId: "univ-seminer-unificate",
+  storageBucket: "univ-seminer-unificate.appspot.com",
+  messagingSenderId: "1030793997557",
+  appId: "1:1030793997557:web:b54826b6ecde22f5fbe9bc",
+  measurementId: "G-QKQ5Q6PX0S",
 };
 
 export default {
-    init() {
-        firebase.initializeApp(firebaseConfig);
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then()
-            .catch(e => {
-                console.log(e)
-            })
-        firebase.analytics();
-    },
-    login(domain) {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        provider.setCustomParameters({
-            hd: `${domain}.ac.jp`, //このドメインのみでログイン可（dc.tohoku.ac.jpはこれのサブドメインなのでログイン可）
-        })
+  init() {
+    firebase.initializeApp(firebaseConfig);
+    firebase
+      .auth()
+      .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+      .then()
+      .catch((e) => {
+        console.log(e);
+      });
+    firebase.analytics();
+  },
+  login() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.setCustomParameters({
+      hd: `tohoku.ac.jp`, //このドメインのみでログイン可（dc.tohoku.ac.jpはこれのサブドメインなのでログイン可）
+    });
 
-        firebase.auth().signInWithPopup(provider)
-            .then(() => {
-                router.push('/').then()
-            })
-            .catch(e => {
-                console.log(e)
-                this.errorMessage = e.message
-                this.ifError = true
-            })
-    },
-    logout() {
-        firebase.auth().signOut()
-            .then(() => {
-                router.push('sign-in').then()
-            })
-            .catch(e => {
-                console.log(e)
-                router.push('sign-in').then()
-            })
-    },
-    onAuth() {
-        firebase.auth().onAuthStateChanged(user => {
-            user = user ? user : {};
-            store.commit('onUserStatusChanged', !!user.uid);
-        });
-    }
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(() => {
+        router.push("/").then();
+      })
+      .catch((e) => {
+        console.log(e);
+        this.errorMessage = e.message;
+        this.ifError = true;
+      });
+  },
+  logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        router.push("sign-in").then();
+      })
+      .catch((e) => {
+        console.log(e);
+        router.push("sign-in").then();
+      });
+  },
+  onAuth() {
+    firebase.auth().onAuthStateChanged((user) => {
+      user = user ? user : {};
+      store.commit("onUserStatusChanged", !!user.uid);
+    });
+  },
 };
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="sign-in">
     <div class="button-wrapper">
-      <b-button block variant="info" @click="googleSignIn('tohoku')" class="sign-in d-flex justify-content-around align-items-center py-3">
-        <img src="../assets/img/google.jpg" width="50" height="50" alt="googleのロゴ"/>
+      <b-button
+        block
+        variant="info"
+        @click="googleSignIn"
+        class="sign-in d-flex justify-content-around align-items-center py-3"
+      >
+        <img
+          src="../assets/img/google.jpg"
+          width="50"
+          height="50"
+          alt="googleのロゴ"
+        />
         <p class="mb-0">東北大アカウントで<br class="d-sm-none" />サインイン</p>
       </b-button>
     </div>
-    <b-alert v-if="isError" dismissible variant="danger" class="mt-2">{{errorMessage}}</b-alert>
-    <b-dd block variant="outline-secondary" text="東北大アカウントとは？" class="button-wrapper">
+    <b-alert v-if="isError" dismissible variant="danger" class="mt-2">{{
+      errorMessage
+    }}</b-alert>
+    <b-dd
+      block
+      variant="outline-secondary"
+      text="東北大アカウントとは？"
+      class="button-wrapper"
+    >
       <b-dd-text class="dd-item">
-        東北大アカウントは、<br/>
+        東北大アカウントは、<br />
         <span class="d-block pt-3">@tohoku.ac.jp(教職員用)</span>
         <span class="d-block pb-3">@dc.tohoku.ac.jp(学生用)</span>
         などのドメインを持つGoogleアカウントです。
@@ -19,36 +36,36 @@
 </template>
 
 <script>
-  import firebase from '../firebase'
+import firebase from "../firebase";
 
-  export default {
-    name: 'SignIn',
-    data() {
-      return {
-        errorMessage: '',
-        isError: false
-      }
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      errorMessage: "",
+      isError: false,
+    };
+  },
+  methods: {
+    googleSignIn() {
+      firebase.login();
     },
-    methods: {
-      googleSignIn(domain) {
-        firebase.login(domain)
-      },
-    },
-  }
+  },
+};
 </script>
 
 <style lang="scss">
-  .button-wrapper {
-    width: 90%;
-    max-width: 500px;
-    margin: 100px auto 0 auto;
+.button-wrapper {
+  width: 90%;
+  max-width: 500px;
+  margin: 100px auto 0 auto;
 
-    .sign-in {
-      font-size: 24px;
-    }
-
-    .dropdown-menu {
-      width: 100%;
-    }
+  .sign-in {
+    font-size: 24px;
   }
+
+  .dropdown-menu {
+    width: 100%;
+  }
+}
 </style>
