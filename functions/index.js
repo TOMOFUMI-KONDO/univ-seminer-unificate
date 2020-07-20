2178247144b82a5ee5f2db8f439bff5dba770832
TOMOFUMI KONDO <ugax2kontomo0314@gmai.com>
2020-07-20 11:47:20 +0900

    イベントを削除するfirebase cloud functionの作成

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,14 @@ const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
 const calendar = google.calendar("v3");
 const functions = require("firebase-functions");
-const cors = require("cors");
+const cors = require("cors")({ origin: true });
 
 const googleCredentials = require("./client_secret_1043408526545-3kk8hmudfnjjsg4nplspaii5361r0eni.apps.googleusercontent.com.json");
+
+const ERROR_RESPONSE_200 = {
+  status: "200",
+  message: "Successful deleting event!",
+};
 
 const ERROR_RESPONSE_500 = {
   status: "500",
@@ -14,36 +19,33 @@ const ERROR_RESPONSE_500 = {
 exports.deleteEventOnCalendar = functions
   .region("asia-northeast1")
   .https.onRequest((request, response) => {
-    console.log(request.headers);
-    const eventId = request.body.eventId;
-    console.log("Receive request body eventId: " + eventId);
+    cors(request, response, () => {
+      console.log(request.headers);
+      const eventId = request.body.eventId;
+      console.log("Receive request body eventId: " + eventId);
 
-    const oAuthClient = new OAuth2(
-      googleCredentials.web.client_id,
-      googleCredentials.web.client_secret,
-      googleCredentials.web.redirect_uris[0]
-    );
+      const oAuthClient = new OAuth2(
+        googleCredentials.web.client_id,
+        googleCredentials.web.client_secret,
+        googleCredentials.web.redirect_uris[0]
+      );
 
-    oAuthClient.setCredentials({
-      refresh_token: googleCredentials.refresh_token,
-    });
-
-    deleteEvent(eventId, oAuthClient)
-      .then((data) => {
-        console.log(data);
-        console.log("Request successful");
-        cors(request, response, () => {
-          response.status(200).send(data);
-        });
-        return;
-      })
-      .catch((error) => {
-        console.error("Error deleting event: " + error.message);
-        cors(request, response, () => {
-          response.status(500).send(ERROR_RESPONSE_500);
-        });
-        return;
+      oAuthClient.setCredentials({
+        refresh_token: googleCredentials.refresh_token,
       });
+
+      return deleteEvent(eventId, oAuthClient)
+        .then(() => {
+          console.log("Request successful");
+          response.status(200).send(ERROR_RESPONSE_200);
+          return;
+        })
+        .catch((error) => {
+          console.error("Error deleting event: " + error.message);
+          response.status(500).send(ERROR_RESPONSE_500);
+          return;
+        });
+    });
   });
 
 const deleteEvent = (eventId, auth) => {
@@ -51,17 +53,14 @@ const deleteEvent = (eventId, auth) => {
     calendar.events.delete(
       {
         auth: auth,
-        calendarId:
-          "tohoku.ac.jp_04dkvupc678baalv9828uu6k4k@group.calendar.google.com",
+        calendarId: "c_5ei73mce5d9am4lc4idap33s50@group.calendar.google.com",
         eventId: eventId,
       },
-      (error, response) => {
+      (error) => {
         if (error) {
-          console.log(error);
           reject(error);
         }
-        console.log(response);
-        resolve(response.data);
+        resolve();
       }
     );
   });
